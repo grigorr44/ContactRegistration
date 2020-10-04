e0ac1b6750114e516d31050c4a59a4575cf81e81
Grigor Grigoryan <grigor@2hz.ai>
2020-10-04 20:09:05 +0400

    Initial Commit

---
+++ Contact Registration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contact Registration
+//
+//  Created by Grigor Grigoryan on 10/4/20.
+//  Copyright © 2020 Grigor Grigoryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
