22e52b9a128ba96fe2ffa8ff6ed3b246953f27b0
Grigor Grigoryan <grigor@2hz.ai>
2020-10-10 19:47:02 +0400

    Support ios12

---
+++ untitled folder/test/test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Grigor Grigoryan on 10/10/20.
+//  Copyright © 2020 Grigor Grigoryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
